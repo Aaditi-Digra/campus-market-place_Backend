ab705599b731a19f7a689fbf08c16f8be0d8df7b
Aaditi <aaditidigra@gmail.com>
2026-06-14 18:14:17 +0530

    fixed

--- app.js
+++ app.js
@@ -16,10 +16,15 @@ const app = express();
 //  middlewares:
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://campus-market-place-frontend.vercel.app"
+    ],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
